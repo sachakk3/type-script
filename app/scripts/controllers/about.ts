30ef96c9e94c7510a2d30988b7f5b0c68e3e5938
sachakk3 <sacha_kk3@list.ru>
2015-10-08 12:21:06 +0300

    First Commit

---
+++ app/scripts/controllers/about.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the firstappApp
+ */
+angular.module('firstappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the firstappApp
+ */
+angular.module('firstappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
